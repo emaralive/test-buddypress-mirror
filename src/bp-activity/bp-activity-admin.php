emaralive/test-buddypress-mirror

commit a5d12a576dc2acf3ec31664f14a87c2529ca9a8e

Activity component: improve PHP code standards using WPCS
See #7228 (trunk)



git-svn-id: https://buddypress.svn.wordpress.org/trunk@13816 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-activity/bp-activity-admin.php
@@ -14,11 +14,12 @@
 defined( 'ABSPATH' ) || exit;
 
 // Include WP's list table class.
-if ( !class_exists( 'WP_List_Table' ) ) require( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+if ( ! class_exists( 'WP_List_Table' ) ) require( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 
 // Per_page screen option. Has to be hooked in extremely early.
-if ( is_admin() && ! empty( $_REQUEST['page'] ) && 'bp-activity' == $_REQUEST['page'] )
+if ( is_admin() && ! empty( $_REQUEST['page'] ) && 'bp-activity' == $_REQUEST['page'] ) {
 	add_filter( 'set-screen-option', 'bp_activity_admin_screen_options', 10, 3 );
+}
 
 /**
  * Register the Activity component admin screen.
@@ -76,26 +77,31 @@ function bp_activity_admin_reply() {
 	$root_id   = ! empty( $_REQUEST['root_id'] )   ? (int) $_REQUEST['root_id']   : 0;
 
 	// $parent_id is required.
-	if ( empty( $parent_id ) )
+	if ( empty( $parent_id ) ) {
 		die( '-1' );
+	}
 
 	// If $root_id not set (e.g. for root items), use $parent_id.
-	if ( empty( $root_id ) )
+	if ( empty( $root_id ) ) {
 		$root_id = $parent_id;
+	}
 
 	// Check that a reply has been entered.
-	if ( empty( $_REQUEST['content'] ) )
-		die( __( 'Error: Please type a reply.', 'buddypress' ) );
+	if ( empty( $_REQUEST['content'] ) ) {
+		die( esc_html__( 'Error: Please type a reply.', 'buddypress' ) );
+	}
 
 	// Check parent activity exists.
 	$parent_activity = new BP_Activity_Activity( $parent_id );
-	if ( empty( $parent_activity->component ) )
-		die( __( 'Error: The item you are trying to reply to cannot be found, or it has been deleted.', 'buddypress' ) );
+	if ( empty( $parent_activity->component ) ) {
+		die( esc_html__( 'Error: The item you are trying to reply to cannot be found, or it has been deleted.', 'buddypress' ) );
+	}
 
 	// @todo: Check if user is allowed to create new activity items
 	// if ( ! current_user_can( 'bp_new_activity' ) )
-	if ( ! bp_current_user_can( 'bp_moderate' ) )
+	if ( ! bp_current_user_can( 'bp_moderate' ) ) {
 		die( '-1' );
+	}
 
 	// Add new activity comment.
 	$new_activity_id = bp_activity_new_comment( array(
@@ -588,10 +594,11 @@ function bp_activity_admin_load() {
 		do_action_ref_array( 'bp_activity_admin_edit_after', array( &$activity, $error ) );
 
 		// If an error occurred, pass back the activity ID that failed.
-		if ( $error )
+		if ( $error ) {
 			$redirect_to = add_query_arg( 'error', $error, $redirect_to );
-		else
+		} else {
 			$redirect_to = add_query_arg( 'updated', $activity->id, $redirect_to );
+		}
 
 		/**
 		 * Filters URL to redirect to after saving.
@@ -688,13 +695,14 @@ function bp_activity_admin_delete() {
 
 			printf(
 				/* translators: 1: activity type. 2: activity author. 3: activity date and time. */
-				__( '"%1$s" activity submitted by %2$s on %3$s', 'buddypress' ),
+				esc_html__( '"%1$s" activity submitted by %2$s on %3$s', 'buddypress' ),
 				esc_html( $activity_type ),
+				// phpcs:ignore WordPress.Security.EscapeOutput
 				bp_core_get_userlink( $activity->user_id ),
 				sprintf(
 					'<a href="%1$s">%2$s</a>',
 					esc_url( bp_activity_get_permalink( $activity->id, $activity ) ),
-					date_i18n( bp_get_option( 'date_format' ), strtotime( $activity->date_recorded ) )
+					esc_html( date_i18n( bp_get_option( 'date_format' ), strtotime( $activity->date_recorded ) ) )
 				)
 			);
 			?>
@@ -760,7 +768,7 @@ function bp_activity_admin_edit() {
 		<h1 class="wp-heading-inline">
 			<?php
 			/* translators: %s: the activity ID */
-			printf( __( 'Editing Activity (ID #%s)', 'buddypress' ), number_format_i18n( (int) $_REQUEST['aid'] ) );
+			printf( esc_html__( 'Editing Activity (ID #%s)', 'buddypress' ), esc_html( number_format_i18n( (int) $_REQUEST['aid'] ) ) );
 			?>
 		</h1>
 
@@ -775,23 +783,27 @@ function bp_activity_admin_edit() {
 						<div id="post-body-content">
 							<div id="postdiv">
 								<div id="bp_activity_action" class="activitybox">
-									<h2><?php _e( 'Action', 'buddypress' ); ?></h2>
+									<h2><?php esc_html_e( 'Action', 'buddypress' ); ?></h2>
 									<div class="inside">
-										<label for="bp-activities-action" class="screen-reader-text"><?php
-											/* translators: accessibility text */
-											_e( 'Edit activity action', 'buddypress' );
-										?></label>
+										<label for="bp-activities-action" class="screen-reader-text">
+											<?php
+												/* translators: accessibility text */
+												esc_html_e( 'Edit activity action', 'buddypress' );
+											?>
+										</label>
 										<?php wp_editor( stripslashes( $activity->action ), 'bp-activities-action', array( 'media_buttons' => false, 'textarea_rows' => 7, 'teeny' => true, 'quicktags' => array( 'buttons' => 'strong,em,link,block,del,ins,img,code,spell,close' ) ) ); ?>
 									</div>
 								</div>
 
 								<div id="bp_activity_content" class="activitybox">
-									<h2><?php _e( 'Content', 'buddypress' ); ?></h2>
+									<h2><?php esc_html_e( 'Content', 'buddypress' ); ?></h2>
 									<div class="inside">
-										<label for="bp-activities-content" class="screen-reader-text"><?php
-											/* translators: accessibility text */
-											_e( 'Edit activity content', 'buddypress' );
-										?></label>
+										<label for="bp-activities-content" class="screen-reader-text">
+											<?php
+												/* translators: accessibility text */
+												esc_html_e( 'Edit activity content', 'buddypress' );
+											?>
+										</label>
 										<?php wp_editor( stripslashes( $activity->content ), 'bp-activities-content', array( 'media_buttons' => false, 'teeny' => true, 'quicktags' => array( 'buttons' => 'strong,em,link,block,del,ins,img,code,spell,close' ) ) ); ?>
 									</div>
 								</div>
@@ -819,9 +831,9 @@ function bp_activity_admin_edit() {
 			<p><?php
 				printf(
 					'%1$s <a href="%2$s">%3$s</a>',
-					__( 'No activity found with this ID.', 'buddypress' ),
+					esc_html__( 'No activity found with this ID.', 'buddypress' ),
 					esc_url( bp_get_admin_url( 'admin.php?page=bp-activity' ) ),
-					__( 'Go back and try again.', 'buddypress' )
+					esc_html__( 'Go back and try again.', 'buddypress' )
 				);
 			?></p>
 
@@ -851,7 +863,7 @@ function bp_activity_admin_edit_metabox_status( $item ) {
 		<div id="minor-publishing">
 			<div id="minor-publishing-actions">
 				<div id="preview-action">
-					<a class="button preview" href="<?php echo esc_attr( bp_activity_get_permalink( $item->id, $item ) ); ?>" target="_blank"><?php _e( 'View Activity', 'buddypress' ); ?></a>
+					<a class="button preview" href="<?php echo esc_url( bp_activity_get_permalink( $item->id, $item ) ); ?>" target="_blank"><?php esc_html_e( 'View Activity', 'buddypress' ); ?></a>
 				</div>
 
 				<div class="clear"></div>
@@ -859,8 +871,8 @@ function bp_activity_admin_edit_metabox_status( $item ) {
 
 			<div id="misc-publishing-actions">
 				<div class="misc-pub-section" id="comment-status-radio">
-					<label class="approved" for="activity-status-approved"><input type="radio" name="activity_status" id="activity-status-approved" value="ham" <?php checked( $item->is_spam, 0 ); ?>><?php _e( 'Approved', 'buddypress' ); ?></label><br />
-					<label class="spam" for="activity-status-spam"><input type="radio" name="activity_status" id="activity-status-spam" value="spam" <?php checked( $item->is_spam, 1 ); ?>><?php _e( 'Spam', 'buddypress' ); ?></label>
+					<label class="approved" for="activity-status-approved"><input type="radio" name="activity_status" id="activity-status-approved" value="ham" <?php checked( $item->is_spam, 0 ); ?>><?php esc_html_e( 'Approved', 'buddypress' ); ?></label><br />
+					<label class="spam" for="activity-status-spam"><input type="radio" name="activity_status" id="activity-status-spam" value="spam" <?php checked( $item->is_spam, 1 ); ?>><?php esc_html_e( 'Spam', 'buddypress' ); ?></label>
 				</div>
 
 				<div class="misc-pub-section curtime misc-pub-section-last">
@@ -872,9 +884,9 @@ function bp_activity_admin_edit_metabox_status( $item ) {
 					<span id="timestamp">
 						<?php
 						/* translators: %s: the date the activity was submitted on */
-						printf( __( 'Submitted on: %s', 'buddypress' ), '<strong>' . $date . '</strong>' );
+						printf( esc_html__( 'Submitted on: %s', 'buddypress' ), '<strong>' . esc_html( $date ) . '</strong>' );
 						?>
-					</span>&nbsp;<a href="#edit_timestamp" class="edit-timestamp hide-if-no-js" tabindex='4'><?php _e( 'Edit', 'buddypress' ); ?></a>
+					</span>&nbsp;<a href="#edit_timestamp" class="edit-timestamp hide-if-no-js" tabindex='4'><?php esc_html_e( 'Edit', 'buddypress' ); ?></a>
 
 					<div id='timestampdiv' class='hide-if-js'>
 						<?php touch_time( 1, 0, 5 ); ?>
@@ -911,12 +923,14 @@ function bp_activity_admin_edit_metabox_status( $item ) {
 function bp_activity_admin_edit_metabox_link( $item ) {
 ?>
 
-	<label class="screen-reader-text" for="bp-activities-link"><?php
-		/* translators: accessibility text */
-		_e( 'Link', 'buddypress' );
-	?></label>
+	<label class="screen-reader-text" for="bp-activities-link">
+		<?php
+			/* translators: accessibility text */
+			esc_html_e( 'Link', 'buddypress' );
+		?>
+	</label>
 	<input type="url" name="bp-activities-link" id="bp-activities-link" value="<?php echo esc_url( $item->primary_link ); ?>" aria-describedby="bp-activities-link-description" />
-	<p id="bp-activities-link-description"><?php _e( 'Activity generated by posts and comments uses the link field for a permalink back to the content item.', 'buddypress' ); ?></p>
+	<p id="bp-activities-link-description"><?php esc_html_e( 'Activity generated by posts and comments uses the link field for a permalink back to the content item.', 'buddypress' ); ?></p>
 
 <?php
 }
@@ -931,10 +945,12 @@ function bp_activity_admin_edit_metabox_link( $item ) {
 function bp_activity_admin_edit_metabox_userid( $item ) {
 ?>
 
-	<label class="screen-reader-text" for="bp-activities-userid"><?php
-		/* translators: accessibility text */
-		_e( 'Author ID', 'buddypress' );
-	?></label>
+	<label class="screen-reader-text" for="bp-activities-userid">
+		<?php
+			/* translators: accessibility text */
+			esc_html_e( 'Author ID', 'buddypress' );
+		?>
+	</label>
 	<input type="number" name="bp-activities-userid" id="bp-activities-userid" value="<?php echo esc_attr( $item->user_id ); ?>" min="1" />
 
 <?php
@@ -1019,8 +1035,8 @@ function bp_activity_admin_edit_metabox_type( $item ) {
 			__FUNCTION__,
 			sprintf(
 				/* translators: %s: the name of the activity type */
-				__( 'This activity item has a type (%s) that is not registered using bp_activity_set_action(), so no label is available.', 'buddypress' ),
-				$selected
+				esc_html__( 'This activity item has a type (%s) that is not registered using bp_activity_set_action(), so no label is available.', 'buddypress' ),
+				esc_html( $selected )
 			),
 			'2.0.0'
 		);
@@ -1030,10 +1046,12 @@ function bp_activity_admin_edit_metabox_type( $item ) {
 
 	?>
 
-	<label for="bp-activities-type" class="screen-reader-text"><?php
-		/* translators: accessibility text */
-		esc_html_e( 'Select activity type', 'buddypress' );
-	?></label>
+	<label for="bp-activities-type" class="screen-reader-text">
+		<?php
+			/* translators: accessibility text */
+			esc_html_e( 'Select activity type', 'buddypress' );
+		?>
+	</label>
 	<select name="bp-activities-type" id="bp-activities-type">
 		<?php foreach ( $actions as $k => $v ) : ?>
 			<option value="<?php echo esc_attr( $k ); ?>" <?php selected( $k,  $selected ); ?>><?php echo esc_html( $v ); ?></option>
@@ -1053,14 +1071,14 @@ function bp_activity_admin_edit_metabox_type( $item ) {
 function bp_activity_admin_edit_metabox_itemids( $item ) {
 ?>
 
-	<label for="bp-activities-primaryid"><?php _e( 'Primary Item ID', 'buddypress' ); ?></label>
+	<label for="bp-activities-primaryid"><?php esc_html_e( 'Primary Item ID', 'buddypress' ); ?></label>
 	<input type="number" name="bp-activities-primaryid" id="bp-activities-primaryid" value="<?php echo esc_attr( $item->item_id ); ?>" min="0" />
 	<br />
 
-	<label for="bp-activities-secondaryid"><?php _e( 'Secondary Item ID', 'buddypress' ); ?></label>
+	<label for="bp-activities-secondaryid"><?php esc_html_e( 'Secondary Item ID', 'buddypress' ); ?></label>
 	<input type="number" name="bp-activities-secondaryid" id="bp-activities-secondaryid" value="<?php echo esc_attr( $item->secondary_item_id ); ?>" min="0" />
 
-	<p><?php _e( 'These identify the object that created this activity. For example, the fields could reference a pair of site and comment IDs.', 'buddypress' ); ?></p>
+	<p><?php esc_html_e( 'These identify the object that created this activity. For example, the fields could reference a pair of site and comment IDs.', 'buddypress' ); ?></p>
 
 <?php
 }
@@ -1155,17 +1173,17 @@ function bp_activity_admin_index() {
 			<?php if ( !empty( $_REQUEST['aid'] ) ) : ?>
 				<?php
 				/* translators: %s: the activity ID */
-				printf( __( 'Activity related to ID #%s', 'buddypress' ), number_format_i18n( (int) $_REQUEST['aid'] ) );
+				printf( esc_html__( 'Activity related to ID #%s', 'buddypress' ), esc_html( number_format_i18n( (int) $_REQUEST['aid'] ) ) );
 				?>
 			<?php else : ?>
-				<?php _ex( 'Activity', 'Admin SWA page', 'buddypress' ); ?>
+				<?php echo esc_html_x( 'Activity', 'Admin SWA page', 'buddypress' ); ?>
 			<?php endif; ?>
 
 			<?php if ( !empty( $_REQUEST['s'] ) ) : ?>
 				<span class="subtitle">
 					<?php
 					/* translators: %s: the activity search terms */
-					printf( __( 'Search results for &#8220;%s&#8221;', 'buddypress' ), wp_html_excerpt( esc_html( stripslashes( $_REQUEST['s'] ) ), 50 ) );
+					printf( esc_html__( 'Search results for &#8220;%s&#8221;', 'buddypress' ), esc_html( wp_html_excerpt( stripslashes( $_REQUEST['s'] ) ), 50 ) );
 					?>
 				</span>
 			<?php endif; ?>
@@ -1175,14 +1193,14 @@ function bp_activity_admin_index() {
 
 		<?php // If the user has just made a change to an activity item, display the status messages. ?>
 		<?php if ( !empty( $messages ) ) : ?>
-			<div id="moderated" class="<?php echo ( ! empty( $_REQUEST['error'] ) ) ? 'error' : 'updated'; ?> notice is-dismissible"><p><?php echo implode( "<br/>\n", $messages ); ?></p></div>
+			<div id="moderated" class="<?php echo ( ! empty( $_REQUEST['error'] ) ) ? 'error' : 'updated'; ?> notice is-dismissible"><p><?php echo implode( "<br/>\n", array_map( 'esc_html', $messages ) ); ?></p></div>
 		<?php endif; ?>
 
 		<?php // Display each activity on its own row. ?>
 		<?php $bp_activity_list_table->views(); ?>
 
 		<form id="bp-activities-form" action="" method="get">
-			<?php $bp_activity_list_table->search_box( __( 'Search all Activity', 'buddypress' ), 'bp-activity' ); ?>
+			<?php $bp_activity_list_table->search_box( esc_html__( 'Search all Activity', 'buddypress' ), 'bp-activity' ); ?>
 			<input type="hidden" name="page" value="<?php echo esc_attr( $plugin_page ); ?>" />
 			<?php $bp_activity_list_table->display(); ?>
 		</form>
@@ -1193,16 +1211,18 @@ function bp_activity_admin_index() {
 				<td colspan="4">
 					<form method="get" action="">
 
-						<h3 id="bp-replyhead"><?php _e( 'Reply to Activity', 'buddypress' ); ?></h3>
-						<label for="bp-activities" class="screen-reader-text"><?php
-							/* translators: accessibility text */
-							_e( 'Reply', 'buddypress' );
-						?></label>
+						<h3 id="bp-replyhead"><?php esc_html_e( 'Reply to Activity', 'buddypress' ); ?></h3>
+						<label for="bp-activities" class="screen-reader-text">
+							<?php
+								/* translators: accessibility text */
+								esc_html_e( 'Reply', 'buddypress' );
+							?>
+						</label>
 						<?php wp_editor( '', 'bp-activities', array( 'dfw' => false, 'media_buttons' => false, 'quicktags' => array( 'buttons' => 'strong,em,link,block,del,ins,img,code,spell,close' ), 'tinymce' => false, ) ); ?>
 
 						<p id="bp-replysubmit" class="submit">
-							<a href="#" class="cancel button-secondary alignleft"><?php _e( 'Cancel', 'buddypress' ); ?></a>
-							<a href="#" class="save button-primary alignright"><?php _e( 'Reply', 'buddypress' ); ?></a>
+							<a href="#" class="cancel button-secondary alignleft"><?php esc_html_e( 'Cancel', 'buddypress' ); ?></a>
+							<a href="#" class="save button-primary alignright"><?php esc_html_e( 'Reply', 'buddypress' ); ?></a>
 
 							<img class="waiting" style="display:none;" src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" alt="" />
 							<span class="error" style="display:none;"></span>

src/bp-activity/bp-activity-embeds.php
@@ -72,9 +72,8 @@ function bp_activity_embed_add_inline_styles() {
 
 	// Grab contents of CSS file and do some rudimentary CSS protection.
 	$css = file_get_contents( $css['file'] );
-	$css = wp_kses( $css, array( "\'", '\"' ) );
 
-	printf( '<style type="text/css">%s</style>', $css );
+	printf( '<style type="text/css">%s</style>', wp_kses( $css, array( "\'", '\"' ) ) );
 }
 add_action( 'embed_head', 'bp_activity_embed_add_inline_styles', 20 );
 
@@ -121,6 +120,8 @@ function bp_activity_embed_has_activity( $activity_id = 0 ) {
  * @since 2.6.0
  */
 function bp_activity_embed_excerpt( $content = '' ) {
+	// Escaping is made in `bp-activity/bp-activity-filters.php`.
+	// phpcs:ignore WordPress.Security.EscapeOutput
 	echo bp_activity_get_embed_excerpt( $content );
 }
 
@@ -317,7 +318,8 @@ EOD;
 		if ( '' !== $content ) {
 			printf( '<div class="bp-activity-embed-display-media %s" style="max-width:%spx">%s</div>',
 				$thumb_width < $float_width ? 'two-col' : 'one-col',
-				$thumb_width < $float_width ? $width : $thumb_width,
+				$thumb_width < $float_width ? intval( $width ) : intval( $thumb_width ),
+				// phpcs:ignore WordPress.Security.EscapeOutput
 				$content
 			);
 		}

src/bp-activity/bp-activity-notifications.php
@@ -392,16 +392,15 @@ function bp_activity_screen_notification_settings() {
 	if ( ! $reply = bp_get_user_meta( bp_displayed_user_id(), 'notification_activity_new_reply', true ) ) {
 		$reply = 'yes';
 	}
-
 	?>
 
 	<table class="notification-settings" id="activity-notification-settings">
 		<thead>
 			<tr>
 				<th class="icon">&nbsp;</th>
-				<th class="title"><?php _e( 'Activity', 'buddypress' ) ?></th>
-				<th class="yes"><?php _e( 'Yes', 'buddypress' ) ?></th>
-				<th class="no"><?php _e( 'No', 'buddypress' )?></th>
+				<th class="title"><?php esc_html_e( 'Activity', 'buddypress' ) ?></th>
+				<th class="yes"><?php esc_html_e( 'Yes', 'buddypress' ) ?></th>
+				<th class="no"><?php esc_html_e( 'No', 'buddypress' )?></th>
 			</tr>
 		</thead>
 
@@ -412,31 +411,49 @@ function bp_activity_screen_notification_settings() {
 					<td>
 						<?php
 						/* translators: %s: the displayed user username */
-						printf( __( 'A member mentions you in an update using "@%s"', 'buddypress' ), bp_members_get_user_slug( bp_displayed_user_id() ) );
+						printf( esc_html__( 'A member mentions you in an update using "@%s"', 'buddypress' ), esc_html( bp_members_get_user_slug( bp_displayed_user_id() ) ) );
 						?>
 					</td>
-					<td class="yes"><input type="radio" name="notifications[notification_activity_new_mention]" id="notification-activity-new-mention-yes" value="yes" <?php checked( $mention, 'yes', true ) ?>/><label for="notification-activity-new-mention-yes" class="bp-screen-reader-text"><?php
-						/* translators: accessibility text */
-						esc_html_e( 'Yes, send email', 'buddypress' );
-					?></label></td>
-					<td class="no"><input type="radio" name="notifications[notification_activity_new_mention]" id="notification-activity-new-mention-no" value="no" <?php checked( $mention, 'no', true ) ?>/><label for="notification-activity-new-mention-no" class="bp-screen-reader-text"><?php
-						/* translators: accessibility text */
-						esc_html_e( 'No, do not send email', 'buddypress' );
-					?></label></td>
+					<td class="yes">
+						<input type="radio" name="notifications[notification_activity_new_mention]" id="notification-activity-new-mention-yes" value="yes" <?php checked( $mention, 'yes', true ) ?>/>
+						<label for="notification-activity-new-mention-yes" class="bp-screen-reader-text">
+							<?php
+								/* translators: accessibility text */
+								esc_html_e( 'Yes, send email', 'buddypress' );
+							?>
+						</label>
+					</td>
+					<td class="no"><input type="radio" name="notifications[notification_activity_new_mention]" id="notification-activity-new-mention-no" value="no" <?php checked( $mention, 'no', true ) ?>/>
+						<label for="notification-activity-new-mention-no" class="bp-screen-reader-text">
+							<?php
+								/* translators: accessibility text */
+								esc_html_e( 'No, do not send email', 'buddypress' );
+							?>
+						</label>
+					</td>
 				</tr>
 			<?php endif; ?>
 
 			<tr id="activity-notification-settings-replies">
 				<td>&nbsp;</td>
-				<td><?php _e( "A member replies to an update or comment you've posted", 'buddypress' ) ?></td>
-				<td class="yes"><input type="radio" name="notifications[notification_activity_new_reply]" id="notification-activity-new-reply-yes" value="yes" <?php checked( $reply, 'yes', true ) ?>/><label for="notification-activity-new-reply-yes" class="bp-screen-reader-text"><?php
-					/* translators: accessibility text */
-					esc_html_e( 'Yes, send email', 'buddypress' );
-				?></label></td>
-				<td class="no"><input type="radio" name="notifications[notification_activity_new_reply]" id="notification-activity-new-reply-no" value="no" <?php checked( $reply, 'no', true ) ?>/><label for="notification-activity-new-reply-no" class="bp-screen-reader-text"><?php
-					/* translators: accessibility text */
-					esc_html_e( 'No, do not send email', 'buddypress' );
-				?></label></td>
+				<td><?php esc_html_e( "A member replies to an update or comment you've posted", 'buddypress' ) ?></td>
+				<td class="yes">
+					<input type="radio" name="notifications[notification_activity_new_reply]" id="notification-activity-new-reply-yes" value="yes" <?php checked( $reply, 'yes', true ) ?>/>
+					<label for="notification-activity-new-reply-yes" class="bp-screen-reader-text">
+						<?php
+							/* translators: accessibility text */
+							esc_html_e( 'Yes, send email', 'buddypress' );
+						?>
+					</label>
+				</td>
+				<td class="no"><input type="radio" name="notifications[notification_activity_new_reply]" id="notification-activity-new-reply-no" value="no" <?php checked( $reply, 'no', true ) ?>/>
+					<label for="notification-activity-new-reply-no" class="bp-screen-reader-text">
+						<?php
+							/* translators: accessibility text */
+							esc_html_e( 'No, do not send email', 'buddypress' );
+						?>
+					</label>
+				</td>
 			</tr>
 
 			<?php

src/bp-activity/classes/class-bp-activity-feed.php
@@ -167,7 +167,7 @@ class BP_Activity_Feed {
 
 			// Check if id is valid.
 			if ( empty( $this->id ) ) {
-				_doing_it_wrong( 'BP_Activity_Feed', __( "RSS feed 'id' must be defined", 'buddypress' ), 'BP 1.8' );
+				_doing_it_wrong( 'BP_Activity_Feed', esc_html__( "RSS feed 'id' must be defined", 'buddypress' ), 'BP 1.8' );
 				return false;
 			}
 
@@ -317,7 +317,7 @@ class BP_Activity_Feed {
 
 				if ( 'activity_comment' == bp_get_activity_action_name() ) :
 			?>
-				<strong><?php _e( 'In reply to', 'buddypress' ) ?></strong> -
+				<strong><?php esc_html_e( 'In reply to', 'buddypress' ) ?></strong> -
 				<?php bp_activity_parent_content() ?>
 			<?php
 				endif;
@@ -413,7 +413,7 @@ class BP_Activity_Feed {
 	 */
 	protected function output() {
 		$this->http_headers();
-		echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?'.'>';
+		echo '<?xml version="1.0" encoding="' . esc_attr( get_option( 'blog_charset' ) ) . '"?'.'>';
 
 		/**
 		 * Fires between the xml and rss tags in an Activity feed.
@@ -439,16 +439,16 @@ class BP_Activity_Feed {
 >
 
 <channel>
-	<title><?php echo $this->title; ?></title>
-	<link><?php echo $this->link; ?></link>
-	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
-	<description><?php echo $this->description ?></description>
-	<lastBuildDate><?php echo mysql2date( 'D, d M Y H:i:s O', bp_activity_get_last_updated(), false ); ?></lastBuildDate>
-	<generator>https://buddypress.org/?v=<?php bp_version(); ?></generator>
+	<title><?php echo esc_html( $this->title ); ?></title>
+	<link><?php echo esc_url( $this->link ); ?></link>
+	<atom:link href="<?php esc_url( self_link() ); ?>" rel="self" type="application/rss+xml" />
+	<description><?php echo esc_html( $this->description ); ?></description>
+	<lastBuildDate><?php echo esc_html( mysql2date( 'D, d M Y H:i:s O', bp_activity_get_last_updated(), false ) ); ?></lastBuildDate>
+	<generator>https://buddypress.org/?v=<?php esc_html( bp_get_version() ); ?></generator>
 	<language><?php bloginfo_rss( 'language' ); ?></language>
-	<ttl><?php echo $this->ttl; ?></ttl>
-	<sy:updatePeriod><?php echo $this->update_period; ?></sy:updatePeriod>
-	<sy:updateFrequency><?php echo $this->update_frequency; ?></sy:updateFrequency>
+	<ttl><?php echo esc_html( $this->ttl ); ?></ttl>
+	<sy:updatePeriod><?php echo esc_html( $this->update_period ); ?></sy:updatePeriod>
+	<sy:updateFrequency><?php echo esc_html( $this->update_frequency ); ?></sy:updateFrequency>
 	<?php
 
 	/**
@@ -462,9 +462,9 @@ class BP_Activity_Feed {
 		<?php while ( bp_activities() ) : bp_the_activity(); ?>
 			<item>
 				<guid isPermaLink="false"><?php bp_activity_feed_item_guid(); ?></guid>
-				<title><?php echo stripslashes( bp_get_activity_feed_item_title() ); ?></title>
+				<title><?php echo esc_html( stripslashes( bp_get_activity_feed_item_title() ) ); ?></title>
 				<link><?php bp_activity_thread_permalink() ?></link>
-				<pubDate><?php echo mysql2date( 'D, d M Y H:i:s O', bp_get_activity_feed_item_date(), false ); ?></pubDate>
+				<pubDate><?php echo esc_html( mysql2date( 'D, d M Y H:i:s O', bp_get_activity_feed_item_date(), false ) ); ?></pubDate>
 
 				<?php if ( bp_get_activity_feed_item_description() ) : ?>
 					<content:encoded><![CDATA[<?php $this->feed_content(); ?>]]></content:encoded>

commit 2d2cd633c86a6dd468e3dbe9a0d720928b5f70c2

Activity Admin: fix an error happening when performing a search
This regression was introduced in r13816.

Props respawnsive

See #9142 (trunk)
Closes https://github.com/buddypress/buddypress/pull/276



git-svn-id: https://buddypress.svn.wordpress.org/trunk@13842 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-activity/bp-activity-admin.php
@@ -1183,7 +1183,7 @@ function bp_activity_admin_index() {
 				<span class="subtitle">
 					<?php
 					/* translators: %s: the activity search terms */
-					printf( esc_html__( 'Search results for &#8220;%s&#8221;', 'buddypress' ), esc_html( wp_html_excerpt( stripslashes( $_REQUEST['s'] ) ), 50 ) );
+					printf( esc_html__( 'Search results for &#8220;%s&#8221;', 'buddypress' ), esc_html( wp_html_excerpt( stripslashes( $_REQUEST['s'] ), 50 ) ) );
 					?>
 				</span>
 			<?php endif; ?>